20613dc1ad71b105a6ff50bd2bd040bfd6399e8c
bellinda <gabriela.angelova@fdiba.tu-sofia.bg>
2018-01-13 16:50:15 +0200

    initial commit for the radix app project
    Signed-off-by: bellinda <gabriela.angelova@fdiba.tu-sofia.bg>

---
+++ app/src/main/java/com/angelova/w510/radixapp/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.angelova.w510.radixapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEmailInput;
+    private EditText mPasswordInput;
+    private Button mLoginBtn;
+    private TextView mForgotPass;
+    private TextView mSignUpNew;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initializeActivity();
+    }
+
+    private void initializeActivity() {
+        mEmailInput = (EditText) findViewById(R.id.email_input);
+        mPasswordInput = (EditText) findViewById(R.id.password_input);
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+        mForgotPass = (TextView) findViewById(R.id.forgot_pass);
+        mSignUpNew = (TextView) findViewById(R.id.sign_up);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        });
+
+        mForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mSignUpNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
